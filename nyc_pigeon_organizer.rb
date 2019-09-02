9ec4bf41e0c05980eb02c717028c07a8612aabf8
Adeja Lendor <alendor6@gmail.com>
2019-09-02 23:39:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,6 @@ def nyc_pigeon_organizer(data)
   data.reduce=({}) do |memo, (attribute, data2)|
     data2.each do |value, names|
       names.each do |name|
-        if !memo.key.include?
+        if !memo.key.include?(name)
+          
 end
