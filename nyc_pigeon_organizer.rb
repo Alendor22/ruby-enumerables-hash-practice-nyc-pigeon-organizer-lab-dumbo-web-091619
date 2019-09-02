e95fb0b451a07f92d8a384c9dee805238d281a60
Adeja Lendor <alendor6@gmail.com>
2019-09-02 23:37:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,28 +1,3 @@
 def nyc_pigeon_organizer(data)
-  hash = {}
-  data.each do |color_gender_lives, values|
-    values.each do |value, array|
-      array.each do |name|
-        if hash[name] = nil 
-          hash[name] = {}
-            hash[name][color_gender_lives] = []
-          else hash[name][color_gender_lives] = []
-        end
-      end
-    end
-end
-hash.each do |name, vaulues|
-  vaulues.each do |hashvalue, array|
-    data.each do |color_gender_lives, vaulues|
-      values.eash do |value, array|
-        array.each do |element|
-          if element == name && hashvalue == color_gender_lives
-            hash[name][hashvalue] << value.t_s 
-          end
-        end
-      end
-    end
-  end
-end
-hash
+  data.reduce=({})
 end
