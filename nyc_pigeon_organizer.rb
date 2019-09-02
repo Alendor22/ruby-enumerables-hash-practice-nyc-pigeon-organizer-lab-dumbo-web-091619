ae1dd68f92cfbbca2713ef5153a34b01d1a60741
Adeja Lendor <alendor6@gmail.com>
2019-09-02 23:25:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,6 @@ def nyc_pigeon_organizer(data)
   data.each do |color_gender_lives, values|
     values.each do |value, array|
       array.each do |name|
-        
+        if hash[name] = nil 
+          
 end
