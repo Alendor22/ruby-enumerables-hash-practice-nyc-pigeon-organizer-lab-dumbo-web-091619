43a9084a19b46744ccda1b5073c8e6ae06673c8e
Adeja Lendor <alendor6@gmail.com>
2019-09-02 23:44:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ data.reduce({}) do |memo, (attribute, data2)|
+    data2.each do |val, names|
+      names.each do |name|
+        if !memo.keys.include?(name)
+          memo[name] = {}
+        end
+        if !memo[name].keys.include?(attribute)
+          memo[name][attribute] = []
+        end
+        memo[name][attribute] << val.to_s
+      end
+    end
+    memo
+  end
 end
